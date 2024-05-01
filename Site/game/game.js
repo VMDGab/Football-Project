155c3be722d93f424d7e69307ae7760e0b256d91
VMDGAB <gmatosdantas777@gmail.com>
2024-05-01 13:45:44 -0300

    questoes dinamicasa

--- Site/game/game.js
+++ Site/game/game.js
@@ -1,3 +1,4 @@
+
 let numQuestao = document.querySelector('#numQuestao')
 let pergunta = document.querySelector('#pergunta')
 
@@ -12,44 +13,14 @@ let nQuestao = 0
 let pontuacao = 0
 let acertos = 0
 
-const q1 = {
-    numQuestao: 1,
-    pergunta: "Quem venceu a copa do mundo de 1970",
-    alternativaA: "Alemanha",
-    alternativaB: "Brasil",
-    alternativaC: "Mexico",
-    alternativaD: "Itália",
-    correta: "Brasil"
-}
-const q2 = {
-    numQuestao: 2,
-    pergunta: "Qual time possui mais titulos da Champions League",
-    alternativaA: "Real Madrid",
-    alternativaB: "Crisciuma",
-    alternativaC: "Milan",
-    alternativaD: "Barcelona",
-    correta: "Real Madrid"
-}
-const q3 = {
-    numQuestao: 3,
-    pergunta: "Quem é o maior artilheiro da história da Champions League",
-    alternativaA: "Henrique Dourado",
-    alternativaB: "Erling Haaland",
-    alternativaC: "Cristiano Ronaldo",
-    alternativaD: "Xavi",
-    correta: "Cristiano Ronaldo"
-}
-
-
-const questoes = [q1, q2, q3]
 
 console.log(questoes.length)
-pergunta.textContent = q1.pergunta
+pergunta.textContent = questoes[nQuestao].pergunta
 
-a.textContent = q1.alternativaA
-b.textContent = q1.alternativaB
-c.textContent = q1.alternativaC
-d.textContent = q1.alternativaD
+a.textContent = questoes[nQuestao].alternativaA
+b.textContent = questoes[nQuestao].alternativaB
+c.textContent = questoes[nQuestao].alternativaC
+d.textContent = questoes[nQuestao].alternativaD
 
 function nextQuestion(nQuestao) {
 
---
+++ Site/game/questions.js
@@ -0,0 +1,30 @@
+const q1 = {
+    numQuestao: 1,
+    pergunta: "Quem venceu a copa do mundo de 1970",
+    alternativaA: "Alemanha",
+    alternativaB: "Brasil",
+    alternativaC: "Mexico",
+    alternativaD: "Itália",
+    correta: "Brasil"
+}
+const q2 = {
+    numQuestao: 2,
+    pergunta: "Qual time possui mais titulos da Champions League",
+    alternativaA: "Real Madrid",
+    alternativaB: "Crisciuma",
+    alternativaC: "Milan",
+    alternativaD: "Barcelona",
+    correta: "Real Madrid"
+}
+const q3 = {
+    numQuestao: 3,
+    pergunta: "Quem é o maior artilheiro da história da Champions League",
+    alternativaA: "Henrique Dourado",
+    alternativaB: "Erling Haaland",
+    alternativaC: "Cristiano Ronaldo",
+    alternativaD: "Xavi",
+    correta: "Cristiano Ronaldo"
+}
+
+
+const questoes = [q1, q2, q3]
